3fc515898227564e17c838abe6e94ff6e4c5973a
williamle8300 <williamle8300@gmail.com>
2014-07-22 16:35:43 -0700

    routine saving...

--- controllers/home.js
+++ controllers/home.js
@@ -9,7 +9,7 @@ exports.index = function(req, res) {
 //	AmazonProducts.getProductDetail({url: 'http://www.amazon.com/dp/B0015X7HD4'}, function(err, productDetail) {
 //		console.log(productDetail);
 //	});
-  res.render('home.html', {
+  res.render('home', {
     title: 'Home'
   });
 };
