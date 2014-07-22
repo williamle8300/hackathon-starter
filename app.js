daf01a3366486eb132a1b66f2488f78c0edab463
williamle8300 <williamle8300@gmail.com>
2014-07-22 16:05:24 -0700

    routine saving...

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ var passport = require('passport');
 var expressValidator = require('express-validator');
 var connectAssets = require('connect-assets');
 
+var nunjucks = require('nunjucks');
+
 /**
  * Controllers (route handlers).
  */
@@ -68,7 +70,8 @@ var csrfExclude = ['/url1', '/url2'];
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+nunjucks.configure('views', {autoescape: true, express: app});
 app.use(compress());
 app.use(connectAssets({
   paths: ['public/css', 'public/js'],
@@ -135,79 +138,7 @@ app.post('/account/password', passportConf.isAuthenticated, userController.postU
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConf.isAuthenticated, userController.getOauthUnlink);
 
-/**
- * API examples routes.
- */
-
-app.get('/api', apiController.getApi);
-app.get('/api/lastfm', apiController.getLastfm);
-app.get('/api/nyt', apiController.getNewYorkTimes);
-app.get('/api/aviary', apiController.getAviary);
-app.get('/api/steam', apiController.getSteam);
-app.get('/api/stripe', apiController.getStripe);
-app.post('/api/stripe', apiController.postStripe);
-app.get('/api/scraping', apiController.getScraping);
-app.get('/api/twilio', apiController.getTwilio);
-app.post('/api/twilio', apiController.postTwilio);
-app.get('/api/clockwork', apiController.getClockwork);
-app.post('/api/clockwork', apiController.postClockwork);
-app.get('/api/foursquare', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getFoursquare);
-app.get('/api/tumblr', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getTumblr);
-app.get('/api/facebook', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getFacebook);
-app.get('/api/github', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getGithub);
-app.get('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getTwitter);
-app.post('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.postTwitter);
-app.get('/api/venmo', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getVenmo);
-app.post('/api/venmo', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.postVenmo);
-app.get('/api/linkedin', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getLinkedin);
-app.get('/api/instagram', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getInstagram);
-app.get('/api/yahoo', apiController.getYahoo);
-
-/**
- * OAuth sign-in routes.
- */
-
-app.get('/auth/instagram', passport.authenticate('instagram'));
-app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/github', passport.authenticate('github'));
-app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect(req.session.returnTo || '/');
-});
-app.get('/auth/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE' }));
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect(req.session.returnTo || '/');
-});
-
-/**
- * OAuth authorization routes for API examples.
- */
-
-app.get('/auth/foursquare', passport.authorize('foursquare'));
-app.get('/auth/foursquare/callback', passport.authorize('foursquare', { failureRedirect: '/api' }), function(req, res) {
-  res.redirect('/api/foursquare');
-});
-app.get('/auth/tumblr', passport.authorize('tumblr'));
-app.get('/auth/tumblr/callback', passport.authorize('tumblr', { failureRedirect: '/api' }), function(req, res) {
-  res.redirect('/api/tumblr');
-});
-app.get('/auth/venmo', passport.authorize('venmo', { scope: 'make_payments access_profile access_balance access_email access_phone' }));
-app.get('/auth/venmo/callback', passport.authorize('venmo', { failureRedirect: '/api' }), function(req, res) {
-  res.redirect('/api/venmo');
-});
+//INSERT BELOW CONTENTS HERE
 
 /**
  * 500 Error Handler.
@@ -224,3 +155,5 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app;
+
+/**[return] * API examples routes.[return] *///app.get('/api', apiController.getApi);[return]app.get('/api/lastfm', apiController.getLastfm);[return]app.get('/api/nyt', apiController.getNewYorkTimes);[return]app.get('/api/aviary', apiController.getAviary);[return]app.get('/api/steam', apiController.getSteam);[return]app.get('/api/stripe', apiController.getStripe);[return]app.post('/api/stripe', apiController.postStripe);[return]app.get('/api/scraping', apiController.getScraping);[return]app.get('/api/twilio', apiController.getTwilio);[return]app.post('/api/twilio', apiController.postTwilio);[return]app.get('/api/clockwork', apiController.getClockwork);[return]app.post('/api/clockwork', apiController.postClockwork);[return]app.get('/api/foursquare', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getFoursquare);[return]app.get('/api/tumblr', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getTumblr);[return]app.get('/api/facebook', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getFacebook);[return]app.get('/api/github', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getGithub);[return]app.get('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getTwitter);[return]app.post('/api/twitter', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.postTwitter);[return]app.get('/api/venmo', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getVenmo);[return]app.post('/api/venmo', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.postVenmo);[return]app.get('/api/linkedin', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getLinkedin);[return]app.get('/api/instagram', passportConf.isAuthenticated, passportConf.isAuthorized, apiController.getInstagram);[return]app.get('/api/yahoo', apiController.getYahoo);[return][return]/**[return] * OAuth sign-in routes.[return] */[return]//app.get('/auth/instagram', passport.authenticate('instagram'));[return]app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), function(req, res) {[return]  res.redirect(req.session.returnTo || '/');[return]});[return]app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));[return]app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), function(req, res) {[return]  res.redirect(req.session.returnTo || '/');[return]});[return]app.get('/auth/github', passport.authenticate('github'));[return]app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {[return]  res.redirect(req.session.returnTo || '/');[return]});[return]app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));[return]app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {[return]  res.redirect(req.session.returnTo || '/');[return]});[return]app.get('/auth/twitter', passport.authenticate('twitter'));[return]app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {[return]  res.redirect(req.session.returnTo || '/');[return]});[return]app.get('/auth/linkedin', passport.authenticate('linkedin', { state: 'SOME STATE' }));[return]app.get('/auth/linkedin/callback', passport.authenticate('linkedin', { failureRedirect: '/login' }), function(req, res) {[return]  res.redirect(req.session.returnTo || '/');[return]});[return]/**[return] * OAuth authorization routes for API examples.[return] */[return]//app.get('/auth/foursquare', passport.authorize('foursquare'));[return]app.get('/auth/foursquare/callback', passport.authorize('foursquare', { failureRedirect: '/api' }), function(req, res) {[return]  res.redirect('/api/foursquare');[return]});[return]app.get('/auth/tumblr', passport.authorize('tumblr'));[return]app.get('/auth/tumblr/callback', passport.authorize('tumblr', { failureRedirect: '/api' }), function(req, res) {[return]  res.redirect('/api/tumblr');[return]});[return]app.get('/auth/venmo', passport.authorize('venmo', { scope: 'make_payments access_profile access_balance access_email access_phone' }));[return]app.get('/auth/venmo/callback', passport.authorize('venmo', { failureRedirect: '/api' }), function(req, res) {[return]  res.redirect('/api/venmo');[return]});[return]
--- controllers/home.js
+++ controllers/home.js
@@ -6,10 +6,10 @@ var AmazonProducts = require('amazon-products');
  */
 
 exports.index = function(req, res) {
-	AmazonProducts.getProductDetail({url: 'http://www.amazon.com/dp/B0015X7HD4'}, function(err, productDetail) {
-		console.log(productDetail);
-	});
-  res.render('home', {
+//	AmazonProducts.getProductDetail({url: 'http://www.amazon.com/dp/B0015X7HD4'}, function(err, productDetail) {
+//		console.log(productDetail);
+//	});
+  res.render('home.html', {
     title: 'Home'
   });
 };
